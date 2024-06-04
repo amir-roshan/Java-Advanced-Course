9bf7f4a57f99ebbe01b83e2ee35706f3dd97cbfd
amir-roshan <amirvalielahi@gmail.com>
2024-06-04 12:02:22 -0700

    Exceptions, lesson 6: The finally block

--- src/main/java/exceptions/ExceptionsDemo.java
+++ src/main/java/exceptions/ExceptionsDemo.java
@@ -1,15 +1,6 @@
 package exceptions;
 
-// Sometimes we see a long list of stack trace because of method calls
-// Sometimes the order of the catch clauses matters
-
-// IOException is the parent class of FileNotFoundException
-// So we can catch IOException instead of FileNotFoundException
-
-// The order matters depending on the hierarchy of the exceptions
-
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
@@ -25,7 +16,6 @@ public class ExceptionsDemo
 			var file = new File("file.txt");
 			new SimpleDateFormat().parse("abc");
 			reader = new FileReader(file);
-			reader.close();
 		}
 		// we can have multiple catch exceptions in one catch block
 		catch(IOException | ParseException e) // it is called c catch block or catch clause`
@@ -35,9 +25,20 @@ public class ExceptionsDemo
 			// we can also get the message of the exception
 			System.out.println(e.getMessage());
 		}
-		//		catch(FileNotFoundException e)
-		//		{
-		//			e.printStackTrace();
-		//		}
+		finally // it always gets executed
+		{
+			if(reader != null)
+			{
+				try
+				{
+					reader.close();
+				}
+				catch(IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+
+		}
 	}
 }
