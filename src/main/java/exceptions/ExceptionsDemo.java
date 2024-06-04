d7d5a1395edb0a92f2ecf5fa361ccc7b2efbf294
amir-roshan <amirvalielahi@gmail.com>
2024-06-04 11:56:38 -0700

    Exceptions, lesson 4 & 5: Catching exceptions and multiple exceptions.

--- src/main/java/exceptions/ExceptionsDemo.java
+++ src/main/java/exceptions/ExceptionsDemo.java
@@ -1,41 +1,43 @@
 package exceptions;
 
-// The Object class is the root of the class hierarchy
+// Sometimes we see a long list of stack trace because of method calls
+// Sometimes the order of the catch clauses matters
 
-// The Exceptions hierarchy:
-// Throwable is the root class of the exceptions hierarchy
-// Throwable has 2 subclasses: Error and Exception
+// IOException is the parent class of FileNotFoundException
+// So we can catch IOException instead of FileNotFoundException
 
-// Error is used to indicate serious problems that a reasonable application should not try to catch
-
-// Exception is used to indicate conditions that a reasonable application might want to catch
-// Exception has 2 subclasses: RuntimeException and IOException
-
-// we can throw an exception manually
-// we use exceptions to handle errors in our code
-
-// In java, we have 3 types of exceptions:
-// 1. Checked exceptions: are checked at compile time, the compiler forces you to handle them
-// It is called checked because the compiler checks if you handle them
-// We should anticipate and handle checked exceptions
-
-// 2. Unchecked exceptions: are not checked at compile time, the compiler does not force you to handle them
-// We got NullPointerException, IllegalArgumentException, IndexOutOfBoundsException, ArithmeticException, etc.
-
-// 3. Errors: are serious problems that should not be caught, they are not meant to be handled
+// The order matters depending on the hierarchy of the exceptions
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo
 {
-	// We make it static so there is no need to create an instance of this class to call this method
-	public static void show() throws FileNotFoundException
+	public static void show()
 	{
-		var file = new File("file.txt");
+		FileReader reader = null;
+		try
+		{
+			var file = new File("file.txt");
+			new SimpleDateFormat().parse("abc");
+			reader = new FileReader(file);
+			reader.close();
+		}
+		// we can have multiple catch exceptions in one catch block
+		catch(IOException | ParseException e) // it is called c catch block or catch clause`
+		{
+			System.out.println("File not found");
 
-		// Compiler forces us to handle the file not found exception
-		var reader = new FileReader(file);
+			// we can also get the message of the exception
+			System.out.println(e.getMessage());
+		}
+		//		catch(FileNotFoundException e)
+		//		{
+		//			e.printStackTrace();
+		//		}
 	}
 }
