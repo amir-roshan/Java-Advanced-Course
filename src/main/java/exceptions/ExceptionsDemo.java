2f97d1124c200c79fc229108bed032c4c62721bc
amir-roshan <amirvalielahi@gmail.com>
2024-06-04 11:17:02 -0700

    Exceptions, lesson2: Types of exceptions

--- src/main/java/exceptions/ExceptionsDemo.java
+++ src/main/java/exceptions/ExceptionsDemo.java
@@ -2,20 +2,29 @@ package exceptions;
 
 // we can throw an exception manually
 // we use exceptions to handle errors in our code
+
+// In java, we have 3 types of exceptions:
+// 1. Checked exceptions: are checked at compile time, the compiler forces you to handle them
+// It is called checked because the compiler checks if you handle them
+// We should anticipate and handle checked exceptions
+
+// 2. Unchecked exceptions: are not checked at compile time, the compiler does not force you to handle them
+// We got NullPointerException, IllegalArgumentException, IndexOutOfBoundsException, ArithmeticException, etc.
+
+// 3. Errors: are serious problems that should not be caught, they are not meant to be handled
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ExceptionsDemo
 {
 	// We make it static so there is no need to create an instance of this class to call this method
-	public static void show()
+	public static void show() throws FileNotFoundException
 	{
-		// Null pointer exception, is the root of many problems in Java
-		// the stack trace is very important to know where the error is,
-		// it shows the method calls that were made before the exception was thrown in reverse order
-		sayHello("null");
-	}
+		var file = new File("file.txt");
 
-	public static void sayHello(final String name)
-	{
-
-		System.out.println("Hello " + name.toUpperCase());
+		// Compiler forces us to handle the file not found exception
+		var reader = new FileReader(file);
 	}
 }
