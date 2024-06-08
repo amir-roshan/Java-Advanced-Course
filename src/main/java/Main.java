739e7541e8b48b23fa949d39da15222feccfc182
amir-roshan <amirvalielahi@gmail.com>
2024-06-07 18:21:38 -0700

    Generics Lesson 6: Type erasure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,20 @@
 import generics.GenericList;
 
+// Bytecode platform is an intermediate code between source code and machine code.
+// Internally the generics are converted to Object type.
+// This is called type erasure, and it acts as non-generic code.
+// If we apply a constraint to a generic type, it will be converted to the constraint type rather than the Object type.
+// The java compiler will take the last constraint type from the left side to use it as the constraint type.
+// This process is called type erasure.
 public class Main
 {
 	public static void main(final String[] args)
 	{
-		// WIth the help of generics we can see the errors at compile time
-		GenericList<Integer> list = new GenericList<Integer>();
-		list.add(2);
+		// With the help of generics we can see the errors at compile time
+		GenericList<Integer> list = new GenericList<>();
+		list.add(2); // this process is called boxing
 
-		int number = list.get(0);
+		int number = list.get(0); // this process is called unboxing
 		System.out.println(number);
 	}
 }
