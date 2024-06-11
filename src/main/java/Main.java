9bec3ac187c321b461750ceca139e03d3dd39377
amir-roshan <amirvalielahi@gmail.com>
2024-06-11 09:25:13 -0700

    Generics Lesson 7: The Comparable Interface

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,26 @@
 import generics.GenericList;
+import generics.User;
 
-// Bytecode platform is an intermediate code between source code and machine code.
-// Internally the generics are converted to Object type.
-// This is called type erasure, and it acts as non-generic code.
-// If we apply a constraint to a generic type, it will be converted to the constraint type rather than the Object type.
-// The java compiler will take the last constraint type from the left side to use it as the constraint type.
-// This process is called type erasure.
 public class Main
 {
 	public static void main(final String[] args)
 	{
-		// With the help of generics we can see the errors at compile time
-		GenericList<Integer> list = new GenericList<>();
-		list.add(2); // this process is called boxing
+		final User user1 = new User(10);
+		final User user2 = new User(20);
 
-		int number = list.get(0); // this process is called unboxing
-		System.out.println(number);
+		System.out.println(user1.compareTo(user2));
+
+		if(user1.compareTo(user2) < 0)
+		{
+			System.out.println("user1 is less than user2");
+		}
+		else if(user1.compareTo(user2) > 0)
+		{
+			System.out.println("user1 is greater than user2");
+		}
+		else
+		{
+			System.out.println("user1 is equal to user2");
+		}
 	}
 }
--- src/main/java/generics/User.java
+++ src/main/java/generics/User.java
@@ -1,5 +1,37 @@
 package generics;
 
-public class User
+public class User implements Comparable<User>
 {
+
+	private int points;
+
+	public User(final int points)
+	{
+		this.points = points;
+	}
+
+	@Override
+	public int compareTo(final User other)
+	{
+
+		return points - other.points;
+
+		// OR
+
+		//	 	 if the current object is less than the object passed as an argument then return -1
+		//		if(points < o.points)
+		//		{
+		//			return -1;
+		//		}
+		//
+		//		if the current object is greater than the object passed as an argument then return 1
+		//		else if(this.points > o.points)
+		//		{
+		//			return 1;
+		//		}
+		//
+		//		if the current object is equal to the object passed as an argument then return 0
+		//		else
+		//			return 0;
+	}
 }
